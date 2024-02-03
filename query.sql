a7c292cdacf7081895eeb7855eba769074c9df9e
Sourav Kumar <144293972+s0uravk@users.noreply.github.com>
2024-02-03 13:52:20 -0500

    Created the ERD and database schema

---
+++ query.sql
@@ -0,0 +1,8 @@
+-- 1.Show all data in campaign table
+SELECT * FROM campaign
+
+-- 2.Show all data in category table
+SELECT * FROM category
+
+-- 3.Show all data in subcategory table
+SELECT * FROM subcategory
